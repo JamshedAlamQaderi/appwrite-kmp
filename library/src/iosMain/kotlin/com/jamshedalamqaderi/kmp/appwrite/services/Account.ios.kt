c66dab0d505db37843cb4b18c40be5588d6f6dcd
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2025-02-10 08:53:03 +0600

    fix: ios launche oAuth2 url

--- library/src/iosMain/kotlin/com/jamshedalamqaderi/kmp/appwrite/services/Account.ios.kt
+++ library/src/iosMain/kotlin/com/jamshedalamqaderi/kmp/appwrite/services/Account.ios.kt
@@ -1,32 +1,44 @@
 package com.jamshedalamqaderi.kmp.appwrite.services
 
 import com.jamshedalamqaderi.kmp.appwrite.OAuthWebViewController
-import com.jamshedalamqaderi.kmp.appwrite.exceptions.AppwriteException
 import platform.UIKit.UIApplication
-import kotlin.coroutines.suspendCoroutine
+
+//actual suspend fun launchOAuth2Url(
+//    authUrl: String,
+//    callbackScheme: String,
+//): String =
+//    suspendCoroutine { cont ->
+//        println("LaunchOAuth2Url")
+//        val oauthVC =
+//            OAuthWebViewController(authUrl, callbackScheme, onResult = { callbackUrl, error ->
+//                println("Callback url: $callbackUrl | Error: $error")
+//                when {
+//                    error != null -> cont.resumeWith(Result.failure(AppwriteException(error)))
+//                    callbackUrl == null ->
+//                        cont
+//                            .resumeWith(Result.failure(AppwriteException("Invalid callback URL")))
+//
+//                    else -> cont.resumeWith(Result.success(callbackUrl))
+//                }
+//            })
+//        val keyWindow = UIApplication.sharedApplication.keyWindow
+//        val rootVC =
+//            keyWindow?.rootViewController
+//                ?: return@suspendCoroutine cont
+//                    .resumeWith(Result.failure(AppwriteException("No root view controller found")))
+//        rootVC.presentViewController(oauthVC, animated = true, completion = null)
+//    }
 
 actual suspend fun launchOAuth2Url(
     authUrl: String,
     callbackScheme: String,
-): String =
-    suspendCoroutine { cont ->
-        println("LaunchOAuth2Url")
-        val oauthVC =
-            OAuthWebViewController(authUrl, callbackScheme, onResult = { callbackUrl, error ->
-                println("Callback url: $callbackUrl | Error: $error")
-                when {
-                    error != null -> cont.resumeWith(Result.failure(AppwriteException(error)))
-                    callbackUrl == null ->
-                        cont
-                            .resumeWith(Result.failure(AppwriteException("Invalid callback URL")))
-
-                    else -> cont.resumeWith(Result.success(callbackUrl))
-                }
-            })
-        val keyWindow = UIApplication.sharedApplication.keyWindow
-        val rootVC =
-            keyWindow?.rootViewController
-                ?: return@suspendCoroutine cont
-                    .resumeWith(Result.failure(AppwriteException("No root view controller found")))
-        rootVC.presentViewController(oauthVC, animated = true, completion = null)
-    }
+): String {
+    val oauthVC =
+        OAuthWebViewController(authUrl, callbackScheme, onResult = { callbackUrl, error ->
+            println("Callback url: $callbackUrl | Error: $error")
+        })
+    val keyWindow = UIApplication.sharedApplication.keyWindow
+    val rootVC = keyWindow?.rootViewController ?: return ""
+    rootVC.presentViewController(oauthVC, animated = true, completion = null)
+    return ""
+}
