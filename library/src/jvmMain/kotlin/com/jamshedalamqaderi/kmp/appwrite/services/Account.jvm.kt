847c19b63b483fec886db921a0840d1e84ab856c
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2025-02-10 09:35:03 +0600

    fix: OAuth2 ios

--- library/src/jvmMain/kotlin/com/jamshedalamqaderi/kmp/appwrite/services/Account.jvm.kt
+++ library/src/jvmMain/kotlin/com/jamshedalamqaderi/kmp/appwrite/services/Account.jvm.kt
@@ -1,4 +1,10 @@
 package com.jamshedalamqaderi.kmp.appwrite.services
 
-actual fun launchOAuth2Url() {
+import com.jamshedalamqaderi.kmp.appwrite.exceptions.AppwriteException
+
+actual suspend fun launchOAuth2Url(
+    authUrl: String,
+    callbackScheme: String,
+): String {
+    throw AppwriteException("OAuth2 functionality is not implemented on desktop yet.")
 }
