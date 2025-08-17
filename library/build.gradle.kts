3b835c103c75fcd0903429cfe73d797f137474dd
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2025-08-17 23:37:49 +0600

    chore: update Maven publish plugin and workflow configuration
    - Upgraded `vanniktech.maven.publish` plugin to version `0.34.0`.
    - Updated MavenCentral publishing configuration in `build.gradle.kts`.
    - Adjusted GitHub workflow to use `publish` task instead of `publishToMavenCentral`.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -98,7 +97,7 @@ android {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
 
     signAllPublications()
     coordinates(group.toString(), "appwrite", version.toString())
